3904f95a9b78e8e043a8733d4dbe1ce203f62729
veradeploy <51812694+veradeploy@users.noreply.github.com>
2025-08-13 15:13:52 +0530

    Update views.get.ts
    Fixed dataset name

--- server/api/stats/views.get.ts
+++ server/api/stats/views.get.ts
@@ -17,7 +17,7 @@ const ViewsQuerySchema = QuerySchema.extend({
 
 function query2sql(query: z.infer<typeof ViewsQuerySchema>, event: H3Event): string {
   const filter = query2filter(query)
-  const { dataset } = useRuntimeConfig(event)
+  const { dataset } = 'sink'
   const sql = select(`formatDateTime(timestamp, '${unitMap[query.unit]}', '${query.clientTimezone}') as time, SUM(_sample_interval) as visits, COUNT(DISTINCT ${logsMap.ip}) as visitors`).from(dataset).where(filter).groupBy('time').orderBy('time')
   appendTimeFilter(sql, query)
   return sql.toString()
